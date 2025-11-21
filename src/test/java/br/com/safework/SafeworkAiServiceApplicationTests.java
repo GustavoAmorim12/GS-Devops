0ec5af435775da147723e5f8b4a0011869d97cf6
gustavoamorim12 <guamorim01@gmail.com>
2025-11-21 01:05:09 -0300

    aaaaaaaaaaa

--- src/test/java/br/com/safework/SafeworkAiServiceApplicationTests.java
+++ src/test/java/br/com/safework/SafeworkAiServiceApplicationTests.java
@@ -3,10 +3,15 @@ package br.com.safework;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import br.com.safework.ai.OllamaImageAnalyzer;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class SafeworkAiServiceApplicationTests {
+
+    @MockBean
+    private OllamaImageAnalyzer ollamaImageAnalyzer;
 
     @Test
     void contextLoads() {
