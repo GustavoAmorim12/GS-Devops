359c88713b833fdf2a6924f2533b29b1d4f47f5a
gustavoamorim12 <guamorim01@gmail.com>
2025-11-21 00:37:52 -0300

    prevenindo erro em testes

--- src/test/java/br/com/safework/SafeworkAiServiceApplicationTests.java
+++ src/test/java/br/com/safework/SafeworkAiServiceApplicationTests.java
@@ -2,12 +2,16 @@ package br.com.safework;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SafeworkAiServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
