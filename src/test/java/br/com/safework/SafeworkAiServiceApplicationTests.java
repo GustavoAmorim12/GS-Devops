77b35db8650ca24a7625c87555124eb4e6eb60dd
gustavoamorim12 <guamorim01@gmail.com>
2025-11-21 00:39:21 -0300

    arrumando novamente

--- src/test/java/br/com/safework/SafeworkAiServiceApplicationTests.java
+++ src/test/java/br/com/safework/SafeworkAiServiceApplicationTests.java
@@ -2,8 +2,6 @@ package br.com.safework;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
